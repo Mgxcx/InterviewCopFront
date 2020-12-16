01fab87abad469b9e2f8043bf2559eda673833f6
Margaux Chevreux <margaux.chevreux@orange.fr>
2020-12-16 15:06:52 +0100

    Use of Store Scores to show them in Detailed Stats

--- screens/InterviewScreenResult.js
+++ screens/InterviewScreenResult.js
@@ -24,6 +24,7 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
   const [listErrorsNewTrophy, setListErrorsNewTrophy] = useState([]);
   const [lastTrophy, setLastTrophy] = useState("");
   const [salary, setSalary] = useState("Aucune donnée disponible");
+  const [categoriesScores, setCategoriesScores] = useState();
   let trophy;
   //pour gérer les polices expo-google-fonts
   let [fontsLoaded] = useFonts({
@@ -38,8 +39,50 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
   //déclenche le setRating au chargement de la page pour récupérer le dernier score enregistré dans Redux
   // pour pouvoir l'afficher ici dans InterviewScreenResult
   useEffect(() => {
-    console.log(detailedscore);
-    //gestion du score
+    //gestion des résultats par catégorie dans les statistiques détaillées
+    var categories = [
+      "Parler de soi",
+      "Storytelling",
+      "Préparatifs de l’entretien",
+      "Projection dans l’entreprise",
+      "Négociation",
+    ];
+
+    var categoriesScores = categories.map((category) => {
+      let indices = [];
+      var idx = detailedscore.category.indexOf(category);
+      while (idx != -1) {
+        indices.push(idx);
+        idx = detailedscore.category.indexOf(category, idx + 1);
+      }
+
+      let scoreCategory;
+      if (indices.length > 0) {
+        scoreCategory = indices.map((indice) => {
+          let indiceScore = detailedscore.score[indice];
+          return indiceScore;
+        });
+      }
+
+      let numberPointsFalse;
+      let numberPointsMax;
+      let sumScoreCategory;
+      if (typeof scoreCategory != "undefined") {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        sumScoreCategory = scoreCategory.reduce(reducer);
+        numberPointsMax = indices.length * 10;
+        numberPointsFalse = numberPointsMax - sumScoreCategory;
+      }
+      return {
+        category,
+        sumScoreCategory,
+        numberPointsMax,
+        numberPointsFalse,
+      };
+    });
+    setCategoriesScores(categoriesScores);
+
+    //gestion du score 5 étoiles
     let newScore5Star = score / 10 / 2;
     setRating(newScore5Star);
     //calcul du salaire d'embauche en récupérant les infos stockées dans redux et en appelant la route du back correspondante
@@ -128,46 +171,81 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
           buttonStyle={styles.button2}
           onPress={toggleOverlay}
         />
-        <Overlay isVisible={overlayVisible} backdropStyle={styles.backdropoverlay} overlayStyle={styles.overlay}>
+        <Overlay isVisible={overlayVisible} overlayStyle={styles.overlay}>
           <View style={styles.overlay}>
             <Text style={styles.title}>Résultats par question</Text>
             <VictoryChart
-              padding={{ top: 5, bottom: 50, left: 50, right: 50 }}
+              padding={{ top: 5, bottom: 40, left: 50, right: 50 }}
               domainPadding={20}
               height={180}
               width={340}
             >
               <VictoryBar
                 style={{
-                  data: { fill: "#E8C518", stroke: "#0773A3", strokeWidth: 2 },
+                  data: { fill: "#E8C518", stroke: "#0773A3", strokeWidth: 1 },
                 }}
                 data={[
-                  { x: "q1", y: 1 },
-                  { x: "q2", y: 2 },
-                  { x: "q3", y: 3 },
-                  { x: "q4", y: 4 },
-                  { x: "q5", y: 5 },
-                  { x: "q6", y: 6 },
-                  { x: "q7", y: 7 },
-                  { x: "q8", y: 8 },
-                  { x: "q9", y: 9 },
-                  { x: "q10", y: 10 },
+                  { x: "q1", y: detailedscore.score[0] },
+                  { x: "q2", y: detailedscore.score[1] },
+                  { x: "q3", y: detailedscore.score[2] },
+                  { x: "q4", y: detailedscore.score[3] },
+                  { x: "q5", y: detailedscore.score[4] },
+                  { x: "q6", y: detailedscore.score[5] },
+                  { x: "q7", y: detailedscore.score[6] },
+                  { x: "q8", y: detailedscore.score[7] },
+                  { x: "q9", y: detailedscore.score[8] },
+                  { x: "q10", y: detailedscore.score[9] },
                 ]}
-                cornerRadius={4}
+                cornerRadius={5}
               />
             </VictoryChart>
             <Text style={styles.title}>Résultats par catégorie</Text>
             <VictoryPie
               data={[
-                { x: "Parler de soi", y: 35 },
-                { x: "Préparatifs", y: 40 },
-                { x: "Storytelling", y: 55 },
-                { x: "Projection", y: 35 },
-                { x: "Négociation", y: 40 },
+                {
+                  x: `Parler de soi \n ${categoriesScores[0].sumScoreCategory}/${categoriesScores[0].numberPointsMax}`,
+                  y: categoriesScores[0].sumScoreCategory,
+                },
+                {
+                  x: " ",
+                  y: categoriesScores[0].numberPointsFalse,
+                },
+                {
+                  x: `Storytelling \n ${categoriesScores[1].sumScoreCategory}/${categoriesScores[1].numberPointsMax}`,
+                  y: categoriesScores[1].sumScoreCategory,
+                },
+                { x: " ", y: categoriesScores[1].numberPointsFalse },
+                {
+                  x: `Préparatifs \n ${categoriesScores[2].sumScoreCategory}/${categoriesScores[2].numberPointsMax}`,
+                  y: categoriesScores[2].sumScoreCategory,
+                },
+                { x: " ", y: categoriesScores[2].numberPointsFalse },
+                {
+                  x: `Projection \n ${categoriesScores[3].sumScoreCategory}/${categoriesScores[3].numberPointsMax}`,
+                  y: categoriesScores[3].sumScoreCategory,
+                },
+                { x: " ", y: categoriesScores[3].numberPointsFalse },
+                {
+                  x: `Négociation \n ${categoriesScores[4].sumScoreCategory}/${categoriesScores[4].numberPointsMax}`,
+                  y: categoriesScores[4].sumScoreCategory,
+                },
+                { x: " ", y: categoriesScores[4].numberPointsFalse },
               ]}
               height={200}
-              padding={{ top: 40, bottom: 40, left: 50, right: 50 }}
-              colorScale={["#ED1C24", "#E8C518", "#FFFEFA", "#0773A3", "#333333"]}
+              padding={{ top: 50, bottom: 50, left: 40, right: 40 }}
+              colorScale={[
+                "#ED1C24",
+                "#ED1C24CC",
+                "#E8C518",
+                "#E8C518CC",
+                "#208C58",
+                "#208C58CC",
+                "#0773A3",
+                "#0773A3CC",
+                "#333333",
+                "#333333CC",
+              ]}
+              cornerRadius={0}
             />
             <Button title="Ok" titleStyle={styles.textbutton2} buttonStyle={styles.button} onPress={toggleOverlay} />
           </View>
@@ -186,7 +264,7 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
           buttonStyle={styles.button3}
           onPress={toggleOverlay2}
         />
-        <Overlay isVisible={overlayVisible2} backdropStyle={styles.backdropoverlay} overlayStyle={styles.overlay}>
+        <Overlay isVisible={overlayVisible2} overlayStyle={styles.overlay}>
           <View style={styles.overlay}>
             <Text style={styles.title}>Conseil n°4.....</Text>
             <Button title="Ok" titleStyle={styles.textbutton2} buttonStyle={styles.button} onPress={toggleOverlay2} />
@@ -198,7 +276,7 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
           buttonStyle={styles.button3}
           onPress={toggleOverlay3}
         />
-        <Overlay isVisible={overlayVisible3} backdropStyle={styles.backdropoverlay} overlayStyle={styles.overlay}>
+        <Overlay isVisible={overlayVisible3} overlayStyle={styles.overlay}>
           <View style={styles.overlay}>
             <Text style={styles.title}>Conseil n°6.....</Text>
             <Button title="Ok" titleStyle={styles.textbutton2} buttonStyle={styles.button} onPress={toggleOverlay3} />
@@ -212,9 +290,11 @@ function InterviewScreenResult({ username, navigation, score, detailedscore, job
             handleSubmitNewTrophy();
           }}
         />
-        <Overlay isVisible={overlayVisible4} backdropStyle={styles.backdropoverlay} overlayStyle={styles.overlay}>
+        <Overlay isVisible={overlayVisible4} overlayStyle={styles.overlay}>
           <View style={styles.overlay}>
-            <Text style={styles.title}>Vous avez gagné le trophée {lastTrophy.name}</Text>
+            <Text style={styles.title}>
+              Vous avez gagné le trophée {"\n"} {lastTrophy.name}
+            </Text>
             <Image source={trophy} style={styles.trophy} />
             {tabErrorsNewTrophy}
             <Button
@@ -292,7 +372,7 @@ const styles = StyleSheet.create({
   },
   button3: {
     marginTop: 15,
-    marginBottom: 10,
+    marginBottom: 5,
     backgroundColor: "#0773A3",
     borderRadius: 15,
     width: 320,
@@ -339,17 +419,16 @@ const styles = StyleSheet.create({
     width: 260,
     textAlign: "center",
   },
-  backdropoverlay: {
+  overlay: {
     backgroundColor: "#4FA2C7",
-    width: "80%",
-    height: "80%",
+    width: "90%",
+    height: "85%",
     borderRadius: 20,
     opacity: 0.95,
     margin: 40,
-  },
-  overlay: {
-    backgroundColor: "transparent",
+    alignSelf: "center",
     alignItems: "center",
+    justifyContent: "center",
   },
   trophy: {
     width: 130,
